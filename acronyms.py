74729d3bb4f2c56c2ecd285124b6e9aa2b4e8861
Hagen Wierstorf <hagen.wierstorf@tu-berlin.de>
2016-01-07 11:51:19 +0100

    Update acronyms

--- acronyms.py
+++ acronyms.py
@@ -1,53 +1,9 @@
 # This file defines all acronyms used in the documentation, see
 # http://twoears.aipa.tu-berlin.de/doc/dev/writing-documentation.html#using-acronyms
 rst_epilog = """
-.. |ACF|             replace:: :abbr:`ACF (Auto-Correlation Function)`
-.. |AGC|             replace:: :abbr:`AGC (Automatic Gain Control)`
-.. |ALSA|            replace:: :abbr:`ALSA (Advanced Linux Sound Architecture)`
-.. |AMS|             replace:: :abbr:`AMS (Amplitude Modulation Spectrogram)`
-.. |ASA|             replace:: :abbr:`ASA (Auditory Scene Analysis)`
-.. |ASR|             replace:: :abbr:`ASR (Automatic Speech Recognition)`
-.. |BASC|            replace:: :abbr:`BASC (Binaural Audio Stream Client)`
-.. |BASS|            replace:: :abbr:`BASS (Binaural Audio Stream Server)`
-.. |BM|              replace:: :abbr:`BM (Basilar Membrane)`
-.. |BRIR|            replace:: :abbr:`BRIR (Binaural Room Impulse Response)`
-.. |BRS|             replace:: :abbr:`BRS (Binaural Room Scaning)`
-.. |CASA|            replace:: :abbr:`CASA (Computational Auditory Scene Analysis)`
-.. |CASP|            replace:: :abbr:`CASP (Computational Auditory Signalprocessing and Perception)`
-.. |CCF|             replace:: :abbr:`CCF (Cross-Correlation Function)`
-.. |DC|              replace:: :abbr:`DC (Direct Current)`
-.. |DRNL|            replace:: :abbr:`DRNL (Dual-Resonance Non-Linear)`
-.. |ERB|             replace:: :abbr:`ERB (Equivalent Rectangular Bandwidth)`
-.. |ETSI|            replace:: :abbr:`ETSI (European Telecommunications Standards Institute)`
-.. |FFT|             replace:: :abbr:`FFT (Fast Fourier Transform)`
-.. |FIFO|            replace:: :abbr:`FIFO (First-In, First-Out)`
-.. |FIR|             replace:: :abbr:`FIR (Finite Impulse Response)`
-.. |GenoM3|          replace:: :abbr:`GenoM3 (Generator of Modules, 3rd version)`
-.. |genomix|         replace:: *genomix*
-.. |HRTF|            replace:: :abbr:`HRTF (Head-Related Transfer Function)`
-.. |IC|              replace:: :abbr:`IC (Interaural Coherence)`
-.. |IHC|             replace:: :abbr:`IHC (Inner Hair Cell)`
-.. |IIR|             replace:: :abbr:`IIR (Infinite Impulse Response)`
-.. |ILD|             replace:: :abbr:`ILD (Interaural Level Difference)`
-.. |ITD|             replace:: :abbr:`ITD (Interaural Time Difference)`
-.. |matlab-genomix|  replace:: *matlab-genomix*
-.. |MOC|             replace:: :abbr:`MOC (Medial Olivo-Cochlear)`
-.. |MU|              replace:: :abbr:`MU (Model Units)`
-.. |NFC-HOA|         replace:: :abbr:'NFC-HOA (Near-Field Compensated Higher Order Ambisonics)`
-.. |OOP|             replace:: :abbr:`OOP (Object-Oriented Programming)`
-.. |RMS|             replace:: :abbr:`RMS (Root Mean Square)`
-.. |robotpkg|        replace:: *robotpkg*
-.. |rosix|           replace:: *rosix*
-.. |ROS|             replace:: :abbr:`ROS (Robot Operating System)`
-.. |SACF|            replace:: :abbr:`SACF (Summary Auto-Correlation Function)`
-.. |SCCF|            replace:: :abbr:`SCCF (Summary Cross-Correlation Function)`
-.. |SCM|             replace:: :abbr:`SCM (Spectral Crest Measure)`
-.. |SFM|             replace:: :abbr:`SFM (Spectral Flatness Measure)`
-.. |SLAM|            replace:: :abbr:`SLAM (Simultaneous Localisation And Mapping)`
-.. |SPL|             replace:: :abbr:`SPL (Sound Pressure Level)`
-.. |STRF|            replace:: :abbr:`STRF (Spectro-Temporal Receptive Field)`
-.. |tcl-genomix|     replace:: *tcl-genomix*
-.. |TF|              replace:: :abbr:`TF (Time-Frequency)`
+.. |NFC-HOA|         replace:: :abbr:`NFC-HOA (Near-Field Compensated Higher Order Ambisonics)`
+.. |HOA|             replace:: :abbr:`HOA (Higher Order Ambisonics)`
+.. |SDM|             replace:: :abbr:`SDM (Spectral Division Method)`
 .. |WFS|             replace:: :abbr:`WFS (Wave Field Synthesis)`
 """
 # vim: textwidth=300:
