3f9b215242339a41570cb91943facfd6b0a80118
Hagen Wierstorf <hagen.wierstorf@tu-berlin.de>
2016-04-05 13:00:16 +0200

    Update theory structure and sidebar menu

--- _include/acronyms.py
+++ _include/acronyms.py
@@ -1,8 +1,9 @@
 # This file defines all acronyms used in the documentation, see
 # http://twoears.aipa.tu-berlin.de/doc/dev/writing-documentation.html#using-acronyms
 rst_epilog = """
-.. |NFC-HOA|         replace:: :abbr:`NFC-HOA (Near-Field Compensated Higher Order Ambisonics)`
 .. |HOA|             replace:: :abbr:`HOA (Higher Order Ambisonics)`
+.. |LSFS|            replace:: :abbr:`LSFS (Local Sound Field Synthesis)`
+.. |NFC-HOA|         replace:: :abbr:`NFC-HOA (Near-Field Compensated Higher Order Ambisonics)`
 .. |SDM|             replace:: :abbr:`SDM (Spectral Division Method)`
 .. |WFS|             replace:: :abbr:`WFS (Wave Field Synthesis)`
 """
