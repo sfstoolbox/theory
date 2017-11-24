671e1769cbd72a7068bef3e13572135aeb30ec57
Hagen Wierstorf <hagen.wierstorf@tu-berlin.de>
2017-11-24 14:00:31 +0100

    Fix \d definition

--- definitions.py
+++ definitions.py
@@ -26,7 +26,7 @@ latexmacros = r"""
     \def \xref             {\x_\text{ref}}
     \def \k                {\vec{k}}
     \def \n                {\vec{n}}
-    \def \d                {\operatorname{d\!}{}}
+    \def \d                {\operatorname{d}\!{}}
     \def \dirac          #1{\operatorname{\delta}\left(#1\right)}
     \def \scalarprod   #1#2{\left\langle#1,#2\right\rangle}
     \def \Hankel     #1#2#3{\mathop{{}H_{#2}^{(#1)}}\!\left(#3\right)}
